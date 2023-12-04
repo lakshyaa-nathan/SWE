d508d2f8bc7819d9ab2fdd5421390a4c1d1e73e0
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2023-12-04 13:14:43 -0600

    hjhjyu

--- reactproject/src/index.js
+++ reactproject/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 
-function Hello(props) {
-  return <h1>Hello World!</h1>;
-}
 
- const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ ReactDOM.render(<App />, document.getElementById('root'));
+
+ 
  /**
 root.render(
   <React.StrictMode>
@@ -19,7 +17,6 @@ root.render(
  */
 
 
-root.render(<Hello />);
 
 
 // If you want to start measuring performance in your app, pass a function
