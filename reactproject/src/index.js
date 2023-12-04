f42d2f498b9e662cb6be3ac9b54eec5a50b27092
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2023-12-04 13:10:24 -0600

    bcgn

--- reactproject/src/index.js
+++ reactproject/src/index.js
@@ -4,31 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ /**
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+ */
+
+
+root.render(<Hello />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
 
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
-}
+
